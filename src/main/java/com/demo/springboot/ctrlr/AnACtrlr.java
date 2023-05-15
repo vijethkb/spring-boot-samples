70e62df01af279ba322e5e56a2934c88b5726e5c
vijeth <vijeth.kb@gmail.com>
2023-05-15 08:56:56 +0530

    Working on Spring Security OAuth2, JWT

---
+++ src/main/java/com/demo/springboot/ctrlr/AnACtrlr.java
@@ -0,0 +1,10 @@
+package com.demo.springboot.ctrlr;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnACtrlr {
+
+	
+	
+}
---
+++ src/main/java/com/demo/springboot/oauth/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.demo.springboot.oauth;
+
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+
+//@Configuration
+public class SecurityConfiguration {
+
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()).httpBasic();
+//		return http.build();
+//	}
+
+}
